0347f46ce1cf0edb572b4e9ac6e1c470b2569656
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-07-12 22:16:39 +0530

    Changes in popup dom element

--- tinymce-editor-integration/src/Components/SmartEditor/index.jsx
+++ tinymce-editor-integration/src/Components/SmartEditor/index.jsx
@@ -1,8 +1,10 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
+import ReactLoading from "react-loading";
 
 export default function SmartEditor() {
   const editorRef = useRef(null);
+  const [grammerChecker, setGrammerChecker] = useState(false);
 
   const handleEditorInit = (evt, editor) => {
     editorRef.current = editor;
@@ -14,17 +16,34 @@ export default function SmartEditor() {
     }
   };
 
-  function openPopup(grammarMistakes, editor, correctedText) {
+  function openPopupGrammarChecker(
+    grammarMistakes,
+    editor,
+    correctedText,
+    count
+  ) {
     // Create the popup element
     let popup = document.createElement("div");
+    popup.id = "popup-grammar-checker-panel";
     popup.className = "popup-grammar-checker";
+
+    // Create the header for count
+    let countText = document.createElement("h3");
+    countText.className = "popup-grammar-checker-header";
+    if (count > 0) {
+      countText.innerHTML = `${count} mistakes found`;
+    } else {
+      countText.innerHTML = `No mistakes found`;
+    }
+    // Append the paragraphs to the popup element
+    popup.appendChild(countText);
 
     grammarMistakes.forEach((mistakes) => {
       let divGrid = document.createElement("div");
       divGrid.className = "div-grid";
       mistakes.forEach((mistake) => {
         // Create the paragraphs
-        var paragraph = document.createElement("p");
+        let paragraph = document.createElement("p");
         paragraph.innerHTML = mistake;
         // Append the paragraphs to the popup element
         divGrid.appendChild(paragraph);
@@ -33,27 +52,29 @@ export default function SmartEditor() {
       popup.appendChild(divGrid);
     });
 
-    // Create the correction grammar button element
-    let correctionButton = document.createElement("button");
-    correctionButton.className = "grammar-correction-button";
-    correctionButton.innerHTML = "Re-correct";
-    correctionButton.onclick = function () {
-      editor.selection.setContent(correctedText);
-      popup.parentNode.removeChild(popup);
-      return false;
-    };
+    if (count > 0) {
+      // Create the correction grammar button element
+      let correctionButton = document.createElement("button");
+      correctionButton.className = "grammar-correction-button";
+      correctionButton.innerHTML = "Re-correct";
+      correctionButton.onclick = function () {
+        editor.selection.setContent(correctedText);
+        popup.remove();
+        return false;
+      };
+
+      // Append the re correct button element to the popup
+      popup.appendChild(correctionButton);
+    }
 
     // Create the close button element
     let closeButton = document.createElement("button");
     closeButton.className = "grammar-correction-close-button";
     closeButton.innerHTML = "Close";
     closeButton.onclick = function () {
-      popup.parentNode.removeChild(popup);
+      popup.remove();
       return false;
     };
-
-    // Append the re correct button element to the popup
-    popup.appendChild(correctionButton);
     // Append the close button element to the popup
     popup.appendChild(closeButton);
     // Append the popup element to the body
@@ -61,7 +82,7 @@ export default function SmartEditor() {
 
     // Close the popup when clicked outside
     document.addEventListener("click", function (event) {
-      if (!popup.contains(event.target)) {
+      if (!popup.contains(event.target) && popup.parentNode) {
         popup.parentNode.removeChild(popup);
       }
     });
@@ -69,13 +90,24 @@ export default function SmartEditor() {
 
   return (
     <>
+      {grammerChecker ? (
+        <ReactLoading
+          type={"spin"}
+          color="#0A99E5"
+          height={100}
+          width={100}
+          className="ReactLoading"
+        />
+      ) : (
+        <></>
+      )}
       <Editor
         apiKey="your-api-key"
         onInit={handleEditorInit}
         initialValue="<p>This is the initial content of the editor.</p>"
         init={{
           height: 500,
-          menubar: false,
+          menubar: true,
           plugins: ["image"],
           toolbar: "image | GrammarChecker",
           setup: (editor) => {
@@ -86,8 +118,8 @@ export default function SmartEditor() {
                 "Highlight a prompt and click this button to query ChatGPT",
               enabled: true,
               onAction: async () => {
+                setGrammerChecker(true);
                 const selection = editor.selection.getContent();
-                console.log(selection);
                 const data = {
                   text: selection,
                 };
@@ -102,7 +134,7 @@ export default function SmartEditor() {
                   }
                 );
                 response.json().then((response) => {
-                  console.log(response.corrected_text);
+                  setGrammerChecker(false);
                   let correctedText = response.corrected_text;
                   let matches = response.matches;
                   let grammarMistakes = [];
@@ -123,14 +155,23 @@ export default function SmartEditor() {
                       incorrectText.substring(end);
 
                     mistakes.push(rearrangedText);
-                    mistakes.push(`Error Type : ` + match[8]);
-                    mistakes.push(`Suggestion : ` + match[1]);
-                    mistakes.push(`<span class="suggestions">` + match[2].slice(0, 2) + "</span>");
+                    mistakes.push(`<b>Error : </b> ` + match[8]);
+                    mistakes.push(`<b>${match[1]}</b> `);
+                    mistakes.push(
+                      `<b>Suggestions : </b> <span class="suggestions">` +
+                        match[2].slice(0, 2) +
+                        "</span>"
+                    );
 
                     grammarMistakes.push(mistakes);
                   }
 
-                  openPopup(grammarMistakes, editor, correctedText);
+                  openPopupGrammarChecker(
+                    grammarMistakes,
+                    editor,
+                    correctedText,
+                    matches.length
+                  );
                 });
               },
             });
