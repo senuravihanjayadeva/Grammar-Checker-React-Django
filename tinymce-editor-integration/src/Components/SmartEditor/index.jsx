52f6b108f51847be6b72c68ae67a0eadc1da80f4
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-07-13 08:35:23 +0530

    Seperate the logic

--- tinymce-editor-integration/src/Components/SmartEditor/index.jsx
+++ tinymce-editor-integration/src/Components/SmartEditor/index.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import ReactLoading from "react-loading";
+import { openPopupGrammarChecker } from "../../Util/grammarchecker";
 
 export default function SmartEditor() {
   const editorRef = useRef(null);
@@ -15,78 +16,6 @@ export default function SmartEditor() {
       console.log(editorRef.current.getContent());
     }
   };
-
-  function openPopupGrammarChecker(
-    grammarMistakes,
-    editor,
-    correctedText,
-    count
-  ) {
-    // Create the popup element
-    let popup = document.createElement("div");
-    popup.id = "popup-grammar-checker-panel";
-    popup.className = "popup-grammar-checker";
-
-    // Create the header for count
-    let countText = document.createElement("h3");
-    countText.className = "popup-grammar-checker-header";
-    if (count > 0) {
-      countText.innerHTML = `${count} mistakes found`;
-    } else {
-      countText.innerHTML = `No mistakes found`;
-    }
-    // Append the paragraphs to the popup element
-    popup.appendChild(countText);
-
-    grammarMistakes.forEach((mistakes) => {
-      let divGrid = document.createElement("div");
-      divGrid.className = "div-grid";
-      mistakes.forEach((mistake) => {
-        // Create the paragraphs
-        let paragraph = document.createElement("p");
-        paragraph.innerHTML = mistake;
-        // Append the paragraphs to the popup element
-        divGrid.appendChild(paragraph);
-      });
-
-      popup.appendChild(divGrid);
-    });
-
-    if (count > 0) {
-      // Create the correction grammar button element
-      let correctionButton = document.createElement("button");
-      correctionButton.className = "grammar-correction-button";
-      correctionButton.innerHTML = "Apply Suggestions";
-      correctionButton.onclick = function () {
-        editor.selection.setContent(correctedText);
-        popup.remove();
-        return false;
-      };
-
-      // Append the re correct button element to the popup
-      popup.appendChild(correctionButton);
-    }
-
-    // Create the close button element
-    let closeButton = document.createElement("button");
-    closeButton.className = "grammar-correction-close-button";
-    closeButton.innerHTML = "Close";
-    closeButton.onclick = function () {
-      popup.remove();
-      return false;
-    };
-    // Append the close button element to the popup
-    popup.appendChild(closeButton);
-    // Append the popup element to the body
-    document.body.appendChild(popup);
-
-    // Close the popup when clicked outside
-    document.addEventListener("click", function (event) {
-      if (!popup.contains(event.target) && popup.parentNode) {
-        popup.parentNode.removeChild(popup);
-      }
-    });
-  }
 
   return (
     <>
@@ -178,7 +107,7 @@ export default function SmartEditor() {
           },
         }}
       />
-      <button onClick={handleLogContent}>Log editor content</button>
+      {/* <button onClick={handleLogContent}>Log editor content</button> */}
     </>
   );
 }
