a8d0ce52535345dc2072a77223ba038adcff4ee1
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-07-13 08:45:37 +0530

    separate grammar checker css

--- tinymce-editor-integration/src/Components/SmartEditor/index.jsx
+++ tinymce-editor-integration/src/Components/SmartEditor/index.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import ReactLoading from "react-loading";
-import { openPopupGrammarChecker } from "../../Util/grammarchecker";
+import { openPopupGrammarChecker } from "../../Utils/grammarchecker";
 
 export default function SmartEditor() {
   const editorRef = useRef(null);
@@ -47,61 +47,66 @@ export default function SmartEditor() {
                 "Highlight a prompt and click this button to query ChatGPT",
               enabled: true,
               onAction: async () => {
-                setGrammerChecker(true);
                 const selection = editor.selection.getContent();
-                const data = {
-                  text: selection,
-                };
-                const response = await fetch(
-                  "http://127.0.0.1:8000/api/grammar-correction/",
-                  {
-                    method: "POST", // *GET, POST, PUT, DELETE, etc.
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(data), // body data type must match "Content-Type" header
-                  }
-                );
-                response.json().then((response) => {
-                  setGrammerChecker(false);
-                  let correctedText = response.corrected_text;
-                  let matches = response.matches;
-                  let grammarMistakes = [];
-                  for (let match of matches) {
-                    let mistakes = [];
-
-                    let incorrectText = match[4];
-                    // Calculate the start and end positions for the span element
-                    let start = match[3];
-                    let end = match[3] + match[6];
-
-                    // Rearrange the incorrectText with the span element
-                    let rearrangedText =
-                      incorrectText.substring(0, start) +
-                      `<span class="incorrecttext">` +
-                      incorrectText.substring(start, end) +
-                      "</span>" +
-                      incorrectText.substring(end);
-
-                    mistakes.push(rearrangedText);
-                    mistakes.push(`<b>Error : </b> ` + match[8]);
-                    mistakes.push(`<b>${match[1]}</b> `);
-                    mistakes.push(
-                      `<b>Suggestions : </b> <span class="suggestions">` +
-                        match[2].slice(0, 2) +
-                        "</span>"
-                    );
-
-                    grammarMistakes.push(mistakes);
-                  }
-
-                  openPopupGrammarChecker(
-                    grammarMistakes,
-                    editor,
-                    correctedText,
-                    matches.length
+                
+                if (selection !== "") {
+                  setGrammerChecker(true);
+                  const data = {
+                    text: selection,
+                  };
+                  const response = await fetch(
+                    "http://127.0.0.1:8000/api/grammar-correction/",
+                    {
+                      method: "POST", // *GET, POST, PUT, DELETE, etc.
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify(data), // body data type must match "Content-Type" header
+                    }
                   );
-                });
+                  response.json().then((response) => {
+                    setGrammerChecker(false);
+                    let correctedText = response.corrected_text;
+                    let matches = response.matches;
+                    let grammarMistakes = [];
+                    for (let match of matches) {
+                      let mistakes = [];
+
+                      let incorrectText = match[4];
+                      // Calculate the start and end positions for the span element
+                      let start = match[3];
+                      let end = match[3] + match[6];
+
+                      // Rearrange the incorrectText with the span element
+                      let rearrangedText =
+                        incorrectText.substring(0, start) +
+                        `<span class="incorrecttext">` +
+                        incorrectText.substring(start, end) +
+                        "</span>" +
+                        incorrectText.substring(end);
+
+                      mistakes.push(rearrangedText);
+                      mistakes.push(`<b>Error : </b> ` + match[8]);
+                      mistakes.push(`<b>${match[1]}</b> `);
+                      mistakes.push(
+                        `<b>Suggestions : </b> <span class="suggestions">` +
+                          match[2].slice(0, 2) +
+                          "</span>"
+                      );
+
+                      grammarMistakes.push(mistakes);
+                    }
+
+                    openPopupGrammarChecker(
+                      grammarMistakes,
+                      editor,
+                      correctedText,
+                      matches.length
+                    );
+                  });
+                } else {
+                  alert("Please select a sentence");
+                }
               },
             });
           },
